ad597dcbc10c8feef875002681c2adab52a21fc9
kurtag01 <kurt.schumaker@gmail.com>
2020-04-19 09:14:06 -0600

    Add files via upload

--- internetcheck.py
+++ internetcheck.py
@@ -14,18 +14,33 @@ def connection():
     return False    
 
 status = connection()
-location = "/etc/config/dhcp"
+location1 = "/etc/config/dhcp"
+location2 = "/etc/config/wireless"
+newline = "\toption disabled '1'\n\toption dns '209.222.18.218 209.222.18.222'"
+
+time.sleep(2)
+
+with open(location1, "r") as file:
+    content = file.readlines()
+    
+content[5] = "\toption rebind_protection '0'\n"
+
+with open(location1, "w") as file"
+    file.writelines(content)
+    
+with open (location2, "a") as file2:
+    file2.writelines(newline)
 
 while status == False:
     time.sleep(5)
     status = connection()
     
-with open(location, "r") as file:
+with open(location1, "r") as file:
     lines = file.readlines()
     
 lines[5] = "\toption rebind_protection '1'\n"
 
-with open (location, "w") as file:
+with open (location1, "w") as file:
     file.writelines(lines)
 
 
